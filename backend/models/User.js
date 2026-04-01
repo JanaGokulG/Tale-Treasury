83f3005029b4fc8043e97a319d7767c936732e90
Jana Gokul G <janagokul2007@gmail.com>
2026-04-01 17:54:29 +0530

    connected the small dashboard to my DB

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    
+    name: {
+    type: String,
+    default: "",
+   },
     email: {
       type: String,
       required: [true, "Email is required"],
